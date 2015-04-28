b5c53a9cf7f907fb46816430c608dd981e6c8c21
Vasile Milea <milea.vasile959@gmail.com>
2015-04-28 19:00:37 +0300

    app.js merged fom V2 to production and bug fix
    - fixed a bug when next or previous buttons used while the player is
    paused and play/pause buttons does not change

--- app/script/app.js
+++ app/script/app.js
@@ -7,210 +7,339 @@ var main = function() {
 	win.on('restore', function() {
 		win.resizeTo(850, 500);
 	});
-	$('#devTools').click(function() {
-		win.showDevTools();
-	});
 
-	// $('#musicSearch').on('change', function(e) {
-	// 	console.log("Event: ", e);
-	// 	console.log($(this).val());
-	// });
-
-	// var tray = new gui.Tray({title: 'Trapster'});
-
-	// Control the options dialog
-	$('#settings').click(function() {
-		$('.modal-settings').removeClass('hide');
-	});
-	$('#settingsClose').click(function() {
-		$('.modal-settings').addClass('hide');
-	});
-
-	var appSettings = {
-			notifications: false
-		},
-		musicList = [],
-		audioSeek = $('#audio-seek'),
+	var currentSong = {
+		name: '',
+		index: 0,
+		path: '',
+		volume: 1,
+		shuffle: false,
+		loop: false
+	};
+	var audio = new Audio();
+	audio.loop = false;
+	var notification;
+	var musicList = [];
+	var audioSeek = $('#audio-seek'),
+		volumeControl = $('#volume-control'),
 		fileList = $('#fileList'),
 		playBtn = $('#play'),
 		pauseBtn = $('#pause'),
 		prevBtn = $('#prev'),
 		nextBtn = $('#next'),
-		volUpBtn = $('#volUp'),
-		volDownBtn = $('#volDown'),
 		getMusicBtn = $('#getMusic'),
 		repeatAll = $('#repeatAll'),
-		repeatTrack = $('#repeatTrack'),
-		currentSong = {
-			name: '',
-			index: 0,
-			path: '',
-			volume: 0.5, 
-			loop: false,
-			shuffle: false
-		},
-		audio = new Audio();
-
-	// Clear playlist
-	var clearPlaylist = function() {
-		$('.playlist ul').empty();
-		$('.playlist ul').append('<h2>No song in playlist</h2>');
-		musicList = [];
-		localStorage.setItem('saved_playlist', '[]');
-	};
-	$('#clearPlaylist').click(function() { clearPlaylist(); });
-
-	// Save playlist
-	var savePlayList = function() {
-		localStorage.setItem('saved_playlist', JSON.stringify(musicList));
-		console.log("Palylist saved!");
-	};
-	$('#savePlaylist').click(function() { savePlayList(); });
-
-	// Stop button
-	var stop = function() {
-		audio.pause();
-		audio.currentTime = 0;
-		$('#play').removeClass('hide');
-		$('#pause').addClass('hide');
-	};
-	$('#stop').click(function() { stop(); });
-
-	// Notifications settings
-	var notificationSettings = function(mode) {
-		if(mode === 'on' && appSettings.notifications === false) {
-			appSettings.notifications = true;
-			$('#notificationOn').addClass('hide');
-			$('#notificationOff').removeClass('hide');
-		} else if(mode === 'off' && appSettings.notifications === true) {
-			appSettings.notifications = false;
-			$('#notificationOn').removeClass('hide');
-			$('#notificationOff').addClass('hide');
+		repeatTrack = $('#repeatTrack');
+	var init = function() {
+		// Persistent settings
+		var loop = localStorage.getItem('player_loop');
+		var volume = localStorage.getItem('player_volume');
+		var shuffle = localStorage.getItem('player_shuffle');
+		if(typeof loop != 'object' && loop != '') {
+			if(loop === 'true') {
+				currentSong.loop = true;
+				audio.loop = true;
+				repeatAll.addClass('hide');
+				repeatTrack.removeClass('hide');
+			} else {
+				currentSong.loop = false;
+				audio.loop = false;
+				repeatAll.removeClass('hide');
+				repeatTrack.addClass('hide');
+			}
 		}
-	};
-	if(appSettings.notifications === true) {
-		$('#notificationOn').addClass('hide');
-		$('#notificationOff').removeClass('hide');
-	} else {
-		$('#notificationOn').removeClass('hide');
-		$('#notificationOff').addClass('hide');
-	}
-	$('#notificationOn').click(function() { notificationSettings('on'); });
-	$('#notificationOff').click(function() { notificationSettings('off'); });
-
-	// Current song title
-	var currentSongTitle = function() {
-		$('.song-title h3').text(currentSong.name);
-		win.title = 'Trapster' + ' - ' + currentSong.name;
-	};
-
-	// Shuffle play
-	var shuffle = function(index) {
-		if(currentSong.shuffle === true) {
-			return Math.floor(Math.random() * (musicList.length - 0 + 1)) + 0;
-		} else {
-			return index;
+		if(typeof shuffle != 'object' && shuffle != '') {
+			if(shuffle === 'true') {
+				currentSong.shuffle = true;
+				$('#shuffle').addClass('active');
+			} else {
+				currentSong.shuffle = false;
+				$('#shuffle').removeClass('active');
+			}
 		}
-	};
-	var shuffleChange = function() {
-		if(currentSong.shuffle === true) {
-			// Stop shuffle
-			$('#shuffle').removeClass('active');
-			currentSong.shuffle = false;
-		} else {
-			// Turn on shuffle
-			$('#shuffle').addClass('active');
-			currentSong.shuffle = true;
+		if(typeof volume != 'object' && volume != '') {
+			volumeControl.val(Number(volume) * 100);
+			audio.volume = Number(volume);
 		}
-	};
-	if(currentSong.shuffle === true) { $('#shuffle').addClass('active'); }
-	$('#shuffle').click(function() { shuffleChange(); })
 
-	// Setting player settings from the currentSong object
-	$('#volume-control').val(currentSong.volume);
-	audio.volume = currentSong.volume;
-	audio.loop = currentSong.loop;
-	if(currentSong.loop) {
-		repeatTrack.removeClass('hide');
-		repeatAll.addClass('hide');
-	} else {
-		repeatTrack.addClass('hide');
-		repeatAll.removeClass('hide');
-	}
-	$('#volume-control').val(currentSong.volume * 100);
-	if(typeof localStorage.getItem('saved_playlist') === 'string') {
-		if(localStorage.getItem('saved_playlist') === '[]') {
+		// Importing saved playlist
+		if(typeof localStorage.getItem('saved_playlist') === 'string') {
+			if(localStorage.getItem('saved_playlist') === '[]') {
+				$('.playlist ul').empty();
+				$('.playlist ul').append('<h2>No song in playlist</h2>');
+			} else {
+				musicList = JSON.parse(localStorage.getItem('saved_playlist'));
+				$('.playlist ul').empty();
+				for(var i = 0; i < musicList.length; i++) {
+					var index = i;
+					var html = '<li data-index="'+index+'"><span class="title">'+musicList[i].name+'</span></li>';
+					$('.playlist ul').append(html);
+				}
+			}
+		} else {musicList = [];}
+	};
+
+	var audioPlayer = {
+		clearPlaylist: function() {
 			$('.playlist ul').empty();
 			$('.playlist ul').append('<h2>No song in playlist</h2>');
-		} else {
-			musicList = JSON.parse(localStorage.getItem('saved_playlist'));
-			$('.playlist ul').empty();
-			for(var i = 0; i < musicList.length; i++) {
-				var index = i;
-				var html = '<li data-index="'+index+'"><span class="title">'+musicList[i].name+'</span></li>';
-				$('.playlist ul').append(html);
+			musicList = [];
+			localStorage.setItem('saved_playlist', '[]');
+		}, 
+		savePlayList: function() {
+			localStorage.setItem('saved_playlist', JSON.stringify(musicList));
+			console.log("Palylist saved!");
+		}, 
+		stop: function() {
+			audio.pause();
+			audio.currentTime = 0;
+			$('.playlist ul li').removeClass('active');
+			$('#play').removeClass('hide');
+			$('#pause').addClass('hide');
+			$('.song-title h3').text('No song currently playing');
+			win.title = 'Trapster';
+		}, 
+		currentSongTitle: function() {
+			$('.song-title h3').text(currentSong.name);
+			win.title = 'Trapster' + ' - ' + currentSong.name;
+		}, 
+		shuffle: function(index) {
+			if(currentSong.shuffle === true) {
+				return Math.floor(Math.random() * (musicList.length - 0 + 1)) + 0;
+			} else {
+				return index;
 			}
-		}
-	} else {musicList = [];}
+		}, 
+		shuffleChange: function() {
+			if(currentSong.shuffle === true) {
+				// Stop shuffle
+				$('#shuffle').removeClass('active');
+				currentSong.shuffle = false;
+				localStorage.setItem('player_shuffle', false);
+			} else {
+				// Turn on shuffle
+				$('#shuffle').addClass('active');
+				currentSong.shuffle = true;
+				localStorage.setItem('player_shuffle', true);
+			}
+		},
+		songDuration: function() {
+			audioSeek.attr("max", parseInt(audio.duration, 10));
+		}, 
+		songLoop: function(mode) {
+			if(mode === 'track') {
+				repeatAll.addClass('hide');
+				repeatTrack.removeClass('hide');
+				localStorage.setItem('player_loop', true);
+				audio.loop = true;
+			} else {
+				repeatAll.removeClass('hide');
+				repeatTrack.addClass('hide');
+				localStorage.setItem('player_loop', false);
+				audio.loop = false;
+			}
+			
+		}, 
+		play: function() {
+			playBtn.addClass('hide');
+			pauseBtn.removeClass('hide');
 
-	// Notifications
-	var notificationsSettings = function(mode, text) {
-		var currSeting = appSettings.notifications;
-		if(mode === 'on' && currSeting === false) {
-			appSettings.notifications = true;
-		} else if(mode === 'off' && currSeting === true) {
-			appSettings.notifications = false;
-		} else { }
-		if(mode === 'display' && appSettings.notifications === true) {
-			var not = new Notification(text);
+			$('.playlist ul li').removeClass('active');
+			$('[data-index="'+currentSong.index+'"]').addClass('active');
+
+			if(currentSong.name === '') {
+				currentSong.name = musicList[0].name;
+				currentSong.index = 0;
+				currentSong.path = musicList[0].path;
+				audio.src = musicList[0].path;
+			}
+
+			audioPlayer.currentSongTitle();
+
+			audio.play();
+		}, 
+		pause: function() {
+			pauseBtn.addClass('hide');
+			playBtn.removeClass('hide');
+
+			$('.playlist ul li').removeClass('active');
+
+			audio.pause();
+		},
+		prev: function() {
+			var prevIndex = currentSong.index - 1;
+
+			prevIndex = audioPlayer.shuffle(prevIndex);
+
+			if(currentSong.index === 0) {
+				prevIndex = musicList.length - 1;
+			} 
+
+			var prev = musicList[prevIndex];
+			currentSong.name = prev.name;
+			currentSong.path = prev.path;
+			currentSong.index = prevIndex;
+			audio.src = prev.path;
+
+			playBtn.addClass('hide');
+			pauseBtn.removeClass('hide');
+
+			$('.playlist ul li').removeClass('active');
+			$('[data-index="'+prevIndex+'"]').addClass('active');
+
+			audioPlayer.currentSongTitle();
+
+			audio.play();
+		},
+		next: function() {
+			var nextIndex = currentSong.index + 1;
+
+			nextIndex = audioPlayer.shuffle(nextIndex);
+
+			if(nextIndex === musicList.length) {
+				nextIndex = 0;
+			}
+
+			var next = musicList[nextIndex];
+			currentSong.name = next.name;
+			currentSong.path = next.path;
+			currentSong.index = nextIndex;
+			audio.src = next.path;
+
+			playBtn.addClass('hide');
+			pauseBtn.removeClass('hide');
+
+			$('.playlist ul li').removeClass('active');
+			$('[data-index="'+nextIndex+'"]').addClass('active');
+
+			audioPlayer.currentSongTitle();
+
+			audio.play();
+		}, 
+		getMusic: function() {
+			fileList.change(function(e){
+				e.preventDefault();
+
+				$('.playlist ul').empty();
+
+				var files = fileList[0].files;
+				for(var i = 0; i < files.length; i++) {
+					musicList.push(files[i]);
+				}
+				for(var i = 0; i < musicList.length; i++) {
+					var index = i;
+					var html = '<li data-index="'+index+'"><span class="title">'+musicList[i].name+'</span></li>';
+					// var datalistHtml = '<option data-index="'+index+'" value="'+musicList[i].name+'">'+index+'</option>';
+					// $('datalist#musicList').append(datalistHtml);
+					$('.playlist ul').append(html);
+				}
+				fileList.val('');
+			});
+			fileList.click();
 		}
 	};
 
-	// Song duration
-	var songDuration = function() {
-		audioSeek.attr("max", parseInt(audio.duration, 10));
-	};
+	$('body').on('click', '*', function() {
+		var id = $(this).attr('id');
+		// console.log(id);
+		switch(id) {
+			case 'prev': 
+				audioPlayer.prev();
+			break;
 
-	// Song loop
-	var songLoop = function(mode) {
-		if(mode === 'track') {
-			repeatAll.addClass('hide');
-			repeatTrack.removeClass('hide');
-			audio.loop = true;
-		} else {
-			repeatAll.removeClass('hide');
-			repeatTrack.addClass('hide');
-			audio.loop = false;
+			case 'play': 
+				audioPlayer.play();
+			break;
+
+			case 'pause': 
+				audioPlayer.pause();
+			break;
+
+			case 'next': 
+				audioPlayer.next();
+			break;
+
+			case 'getMusic': 
+				audioPlayer.getMusic();
+			break;
+
+			case 'repeatAll': 
+				audioPlayer.songLoop('track');
+			break;
+
+			case 'repeatTrack': 
+				audioPlayer.songLoop();
+			break;
+
+			case 'shuffle': 
+				audioPlayer.shuffleChange();
+			break;
+
+			case 'stop': 
+				audioPlayer.stop();
+			break;
+
+			case 'savePlaylist': 
+				audioPlayer.savePlayList();
+			break;
+
+			case 'clearPlaylist': 
+				audioPlayer.clearPlaylist();
+			break;
+
+			case 'settings': 
+				$('.modal-settings').removeClass('hide');
+			break;
+
+			case 'devTools': 
+				win.showDevTools();
+			break;
+
+			case 'settingsClose': 
+				$('.modal-settings').addClass('hide');
+			break;
+
+			case 'settingsSave': 
+				
+			break;
 		}
-	};
-	repeatAll.click(function() { songLoop('track'); });
-	repeatTrack.click(function() { songLoop(); });
-
-	// Get music list
-	getMusicBtn.click(function() {
-		fileList.change(function(e){
-			e.preventDefault();
-
-			$('.playlist ul').empty();
-
-			var files = fileList[0].files;
-			for(var i = 0; i < files.length; i++) {
-				musicList.push(files[i]);
-			}
-			for(var i = 0; i < musicList.length; i++) {
-				var index = i;
-				var html = '<li data-index="'+index+'"><span class="title">'+musicList[i].name+'</span></li>';
-				// var datalistHtml = '<option data-index="'+index+'" value="'+musicList[i].name+'">'+index+'</option>';
-				// $('datalist#musicList').append(datalistHtml);
-				$('.playlist ul').append(html);
-			}
-			notificationsSettings('display', "Music added to your list");
-			fileList.val('');
-		});
-		fileList.click();
 	});
 
-	// Player select
+	// Audio player tray
+	var tray = new gui.Tray({title: 'Trapster', icon: 'trapster-title.png'});
+	var trayMenu = new gui.Menu();
+	trayMenu.append(new gui.MenuItem({
+		label: 'Play', 
+		icon: 'app/media/buttons/play.png', 
+		click: function() {
+			audioPlayer.play();
+		}
+	}));
+	trayMenu.append(new gui.MenuItem({
+		label: 'Previous', 
+		icon: 'app/media/buttons/prev.png', 
+		click: function() {
+			audioPlayer.prev();
+		}
+	}));
+	trayMenu.append(new gui.MenuItem({
+		label: 'Next', 
+		icon: 'app/media/buttons/next.png', 
+		click: function() {
+			audioPlayer.next();
+		}
+	}));
+	trayMenu.append(new gui.MenuItem({
+		label: 'Stop', 
+		icon: 'app/media/buttons/stop.png', 
+		click: function() {
+			audioPlayer.stop();
+		}
+	}));
+
+	tray.menu = trayMenu;
+
+	// Play the song selected from the playlist
 	$('.playlist ul').on('click', 'li', function(e) {
 		e.preventDefault();
 
@@ -224,8 +353,8 @@ var main = function() {
 		currentSong.name = musicList[index].name;
 		currentSong.path = musicList[index].path;
 		
-		currentSongTitle();
-		notificationsSettings('display', "Playing " + currentSong.name);
+		audioPlayer.currentSongTitle();
+		// notificationsSettings('display', "Playing " + currentSong.name);
 		audio.play();
 
 		$('.playlist ul li').removeClass('active');
@@ -233,134 +362,19 @@ var main = function() {
 	});
 
 	// Volume control
-	$('#volume-control').on('change', function() {
+	volumeControl.on('change', function() {
 		var volume = $('#volume-control').val() / 100;
+		localStorage.setItem('player_volume', volume);
 		currentSong.volume = volume;
 		audio.volume = volume;
 	});
 
-	// Play control
-	var playerPlay = function() {
-		playBtn.addClass('hide');
-		pauseBtn.removeClass('hide');
-
-		$('.playlist ul li').removeClass('active');
-		$('[data-index="'+currentSong.index+'"]').addClass('active');
-
-		if(currentSong.name === '') {
-			currentSong.name = musicList[0].name;
-			currentSong.index = 0;
-			currentSong.path = musicList[0].path;
-			audio.src = musicList[0].path;
-		}
-
-		currentSongTitle();
-		notificationsSettings('display', "Playing " + currentSong.name);
-
-		audio.play();
-	};
-	playBtn.click(function(e) { playerPlay(); });
-
-	// Pause control
-	var playerPause = function() {
-		pauseBtn.addClass('hide');
-		playBtn.removeClass('hide');
-
-		$('.playlist ul li').removeClass('active');
-
-		audio.pause();
-	};
-	pauseBtn.click(function(e) { playerPause(); });
-
-	// Prev control
-	var playerPrev = function() {
-		var prevIndex = currentSong.index - 1;
-
-		prevIndex = shuffle(prevIndex);
-
-		if(currentSong.index === 0) {
-			prevIndex = musicList.length - 1;
-		} 
-
-		var prev = musicList[prevIndex];
-		currentSong.name = prev.name;
-		currentSong.path = prev.path;
-		currentSong.index = prevIndex;
-		audio.src = prev.path;
-
-		$('.playlist ul li').removeClass('active');
-		$('[data-index="'+prevIndex+'"]').addClass('active');
-
-		currentSongTitle();
-		notificationsSettings('display', "Playing " + currentSong.name);
-
-		audio.play();
-	};
-	prevBtn.click(function(e) { playerPrev(); });
-
-	// Next control
-	var playerNext = function() {
-		var nextIndex = currentSong.index + 1;
-
-		nextIndex = shuffle(nextIndex);
-
-		if(nextIndex === musicList.length) {
-			nextIndex = 0;
-		}
-
-		var next = musicList[nextIndex];
-		currentSong.name = next.name;
-		currentSong.path = next.path;
-		currentSong.index = nextIndex;
-		audio.src = next.path;
-
-		$('.playlist ul li').removeClass('active');
-		$('[data-index="'+nextIndex+'"]').addClass('active');
-
-		currentSongTitle();
-		notificationsSettings('display', "Playing " + currentSong.name);
-
-		audio.play();
-	}
-	nextBtn.click(function() { playerNext(); });
-
-	// Remote control functionality
-	// peer.on('connection', function(conn) {
-	// 	conn.on('data', function(command){
-	//  		switch(command) {
-	//  			case 'play':
-	//  				playerPlay();
- // 				break;
-
- // 				case 'pause':
-	//  				playerPause();
- // 				break;
-
- // 				case 'next':
-	//  				playerNext();
- // 				break;
-
- // 				case 'prev':
-	//  				playerPrev();
- // 				break;
-
- // 				case 'volUp':
-	//  				playerVolUp();
- // 				break;
-
- // 				case 'volDown':
-	//  				playerVolDown();
- // 				break;
-	//  		}
-	// 	});
-	// });
-
 	// Checking if the current song is finished
 	setInterval(function() {
 		if(audio.ended) { 
-			playerNext();
+			audioPlayer.next();
 		}
-		songDuration();
+		audioPlayer.songDuration();
 	}, 1000);
 
 	// Updating the seek bar
@@ -372,6 +386,9 @@ var main = function() {
 	audioSeek.on('change', function() {
 		audio.currentTime = audioSeek.val();
 	});
+
+	// Initializing some features of player
+	init();
 };
 
 $(document).ready(main);
